2e5c7c31c8f0be31b156ac660396cd1a5612282f
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-03-20 22:10:55 +0000

    Enumeration safe removal of table objects

--- scripts/OreConverter.lua
+++ scripts/OreConverter.lua
@@ -1,19 +1,19 @@
-OreConverter = {}
-acceptedOreConversions = {
-							{from="iron-ore", to="ax-matter-iron-ore"},
-							{from="copper-ore", to="ax-matter-copper-ore"},
-							{from="stone", to="ax-matter-stone"},
-							{from="coal", to="ax-matter-coal"},
-						 }
-						 
+OreConverter = {}					 
 OreConverterProductsNeeded = 4
 BoxSize = 10
 
-function OreConverter.script_on_nth_tick()
-	for k,x in pairs(global.axmatter.oreConverters) do
+local function starts_with(str, start)
+   return str:sub(1, #start) == start
+end
+
+function OreConverter.script_on_nth_tick()	
+	for k=#global.axmatter.oreConverters, 1, -1 do
+		local x = global.axmatter.oreConverters[k]
+		
 		if ( x.entity.valid and x.ore.valid ) then
 			local productsFinished = x.entity.products_finished
 			local percentage = productsFinished / OreConverterProductsNeeded * 100
+						
 			if ( percentage >= 100 ) then 
 				local convertToOre = OreConverter.getConvertToOre(x.ore.name) --Get what ore we are converting to
 				x.entity.active = false --disable this entity
@@ -22,7 +22,7 @@ function OreConverter.script_on_nth_tick()
 				
 				-- Convert the ore
 				for _,oreEnt in ipairs(x.entity.surface.find_entities_filtered{area=oreArea, name=x.ore.name}) do
-					local ent=oreEnt.surface.create_entity{force="neutral", name=convertToOre, position=oreEnt.position}
+					local ent=oreEnt.surface.create_entity{force="neutral", name=convertToOre.name, position=oreEnt.position}
 					ent.amount=oreEnt.amount
 					oreEnt.destroy()
 				end
@@ -34,7 +34,8 @@ function OreConverter.script_on_nth_tick()
 				end
 			end
 		else
-			OreConverter.Cleanup(x.entity) --attempt cleanup
+			game.print("AXMatter: Cleaning up a bad ore converter, I can't tell you where it was, just that it was very naughty!")
+			for _,renderData in pairs(x.renderdata) do rendering.destroy(renderData) end
 			table.remove(global.axmatter.oreConverters, k)
 		end
 	end
@@ -54,7 +55,6 @@ function OreConverter.script_on_entity_placed(event)
 	local entity = event.created_entity	
 	local player = nil
 	if not isRobot then player = game.players[event.player_index] end
-
 	if ( entity ) then
 		if entity.valid and entity.name == "ax-matter-ore-converter" then
 			-- check to see if the machine was placed on ore
@@ -67,7 +67,7 @@ function OreConverter.script_on_entity_placed(event)
 				end
 				return
 			else
-				if ( not OreConverter.isOreAllowed(oreFinderResult[1].name) ) then
+				if ( not OreConverter.getConvertToOre(oreFinderResult[1].name) ) then
 					if not isRobot then
 						player.print({"", oreFinderResult[1].localised_name, " is not a supported ore"})
 						player.get_main_inventory().insert({name=entity.name, count=1})
@@ -98,25 +98,25 @@ function OreConverter.script_on_entity_placed(event)
 	end
 end
 
-function OreConverter.isOreAllowed(val)
-	for _,x in ipairs(acceptedOreConversions) do
-		if ( x.from == val ) then return true end		
-	end
+function OreConverter.GetOreName(val)
+	local oreName = 'ax-matter-' .. val
+	-- Ignore Infinite ore types, just output their normal ore counterparts, if they exist.
+	--if ( starts_with(val,"infinite-") ) then
+	--	oreName = "ax-matter-" .. val:sub(10)		--Remove the infinate part
+	--end
 	
-	return false
+	return oreName
 end
 
 function OreConverter.getConvertToOre(val)
-	for _,x in ipairs(acceptedOreConversions) do
-		if ( x.from == val ) then return x.to end		
-	end
-	
-	return nil
+	return game.entity_prototypes[OreConverter.GetOreName(val)]
 end
 
 function OreConverter.Cleanup(entity)
-	for k,x in pairs(global.axmatter.oreConverters) do
+	for k=#global.axmatter.oreConverters, 1, -1 do
+		local x = global.axmatter.oreConverters[k]
 		if ( x.entity == entity ) then
+			x.entity.surface.create_entity{name="flying-text", position=x.entity.position, text="Ore Converted", color={r=0.5,g=1,b=1}}
 			for _,renderData in pairs(x.renderdata) do
 				rendering.destroy(renderData)
 				table.remove(global.axmatter.oreConverters, k)
