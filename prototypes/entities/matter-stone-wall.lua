260342111e826dc35f74ec68b51aa68432857d34
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-04-05 21:15:14 +0100

    New graphics for Self Healing Stone Wall

--- prototypes/entities/matter-stone-wall.lua
+++ prototypes/entities/matter-stone-wall.lua
@@ -7,34 +7,13 @@ matterWall.max_health = 500
 matterWall.minable.result = matterWall.name
 
 matterWall.resistances = {
-      {
-        type = "physical",
-        decrease = 3,
-        percent = 35
-      },
-      {
-        type = "impact",
-        decrease = 45,
-        percent = 68
-      },
-      {
-        type = "explosion",
-        decrease = 10,
-        percent = 45
-      },
-      {
-        type = "fire",
-        percent = 100
-      },
-      {
-        type = "acid",
-        percent = 80
-      },
-      {
-        type = "laser",
-        percent = 78
-      }
-    }
+    {type = "physical", decrease = 3, percent = 35},
+    {type = "impact", decrease = 45, percent = 68},
+    {type = "explosion", decrease = 10, percent = 45},
+    {type = "fire", percent = 100},
+    {type = "acid", percent = 80},
+    {type = "laser", percent = 78}
+}
 
 matterWall.pictures.single.layers[1].filename = gfxPath .. "/wall-single.png"
 matterWall.pictures.single.layers[1].hr_version.filename = gfxPath .. "/hr-wall-single.png"
@@ -69,9 +48,41 @@ matterWall.pictures.water_connection_patch.sheets[1].hr_version.filename = gfxPa
 matterWall.pictures.gate_connection_patch.sheets[1].filename = gfxPath .. "/wall-gate.png"
 matterWall.pictures.gate_connection_patch.sheets[1].hr_version.filename = gfxPath .. "/hr-wall-gate.png"
 
-
 -- Self healing walls
 local selfHealingWall = util.table.deepcopy(matterWall)
 selfHealingWall.name = "ax-matter-self-healing-wall"
+
+selfHealingWall.pictures.single.layers[1].filename = gfxPath .. "/self-repair/wall-single.png"
+selfHealingWall.pictures.single.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-single.png"
+
+selfHealingWall.pictures.straight_vertical.layers[1].filename = gfxPath .. "/self-repair/wall-vertical.png"
+selfHealingWall.pictures.straight_vertical.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-vertical.png"
+
+selfHealingWall.pictures.straight_horizontal.layers[1].filename = gfxPath .. "/self-repair/wall-horizontal.png"
+selfHealingWall.pictures.straight_horizontal.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-horizontal.png"
+
+selfHealingWall.pictures.corner_right_down.layers[1].filename = gfxPath .. "/self-repair/wall-corner-right.png"
+selfHealingWall.pictures.corner_right_down.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-corner-right.png"
+
+selfHealingWall.pictures.corner_left_down.layers[1].filename = gfxPath .. "/self-repair/wall-corner-left.png"
+selfHealingWall.pictures.corner_left_down.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-corner-left.png"
+
+selfHealingWall.pictures["t_up"].layers[1].filename = gfxPath .. "/self-repair/wall-t.png"
+selfHealingWall.pictures["t_up"].layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-t.png"
+
+selfHealingWall.pictures.ending_right.layers[1].filename = gfxPath .. "/self-repair/wall-ending-right.png"
+selfHealingWall.pictures.ending_right.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-ending-right.png"
+
+selfHealingWall.pictures.ending_left.layers[1].filename = gfxPath .. "/self-repair/wall-ending-left.png"
+selfHealingWall.pictures.ending_left.layers[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-ending-left.png"
+
+selfHealingWall.pictures.filling.filename = gfxPath .. "/self-repair/wall-filling.png"
+selfHealingWall.pictures.filling.hr_version.filename = gfxPath .. "/self-repair/hr-wall-filling.png"
+
+selfHealingWall.pictures.water_connection_patch.sheets[1].filename = gfxPath .. "/self-repair/wall-patch.png"
+selfHealingWall.pictures.water_connection_patch.sheets[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-patch.png"
+
+selfHealingWall.pictures.gate_connection_patch.sheets[1].filename = gfxPath .. "/self-repair/wall-gate.png"
+selfHealingWall.pictures.gate_connection_patch.sheets[1].hr_version.filename = gfxPath .. "/self-repair/hr-wall-gate.png"
 
 data:extend({matterWall, selfHealingWall})
