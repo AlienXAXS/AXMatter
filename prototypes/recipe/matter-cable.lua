4726dc2c1380db75332399e0e134cd4969422426
AlienXAXS <mark.phillips@axsemail.co.uk>
2019-04-07 01:13:00 +0100

    Switched Matter Cable to use Copper instead of Iron

--- prototypes/recipe/matter-cable.lua
+++ prototypes/recipe/matter-cable.lua
@@ -2,7 +2,7 @@ data:extend({
     {
         type = "recipe",
         name = "ax-matter-cable",
-        ingredients = {{"ax-matter-iron-plate", 1}},
+        ingredients = {{"ax-matter-copper-plate", 1}},
         result = "ax-matter-cable",
         enabled = false,
         result_count = 2
